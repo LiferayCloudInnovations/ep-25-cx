4b41debe2f70f0b403ff9fa97ed7f4230908840e
Raymond Augé <raymond.auge@liferay.com>
2025-07-03 10:40:23 -0400

    make fields with defaults optional

--- topics-exchange-workspace/modules/portal-trebuchet-api/src/main/java/com/liferay/portal/trebuchet/configuration/MessageBrokerConfiguration.java
+++ topics-exchange-workspace/modules/portal-trebuchet-api/src/main/java/com/liferay/portal/trebuchet/configuration/MessageBrokerConfiguration.java
@@ -9,13 +9,13 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
         id = "com.liferay.portal.trebuchet.configuration.MessageBrokerConfiguration"
 )
 public interface MessageBrokerConfiguration {
-    @Meta.AD(deflt = "localhost")
+    @Meta.AD(deflt = "localhost", required = false)
     String host();
 
-    @Meta.AD(deflt = "5672")
+    @Meta.AD(deflt = "5672", required = false)
     int port();
 
-    @Meta.AD(deflt = "false")
+    @Meta.AD(deflt = "false", required = false)
     boolean automaticRecoveryEnabled();
 
     String username();

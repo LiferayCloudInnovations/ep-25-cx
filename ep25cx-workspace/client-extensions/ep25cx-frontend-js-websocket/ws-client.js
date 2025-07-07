7e364b7d91be424bcadad7d1e99460b61ab3dfc7
Raymond Augé <raymond.auge@liferay.com>
2025-07-07 16:02:00 -0400

    sample js that runs in the browser console

---
+++ ep25cx-workspace/client-extensions/ep25cx-frontend-js-websocket/ws-client.js
@@ -0,0 +1,23 @@
+var client;
+import('https://ga.jspm.io/npm:@stomp/stompjs@7.0.0/esm6/index.js').then((mod) => {
+  console.log(mod);
+
+  client = new mod.Client({
+    brokerURL: `ws://${location.hostname}:15674/ws`,
+    connectHeaders: {
+      login: 'lfrrabbitmq',
+      passcode: 'lfrrabbitmq',
+	  host: location.hostname,
+    },
+    onConnect: () => {
+      client.subscribe('C_EP25SampleEvent', message =>
+        console.log(`Received: ${message.body}`)
+      );
+      client.publish({ destination: 'C_EP25SampleEvent', body: 'Response Message' });
+    },
+  });
+
+  client.activate();
+});
+
+client.deactivate();

6da520d61ccbd179fd57dc7d5a5bc043849445f9
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2025-07-08 09:25:59 -0500

    Add node.js message consumer

---
+++ ep25cx-workspace/client-extensions/liferay-sample-etc-node/app.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+var express = require('express')
+
+var connectOptions = {
+    "hostname": "liferay-default-rabbitmq",
+    "username": "lfrrabbitmq",
+    "password": "lfrrabbitmq",
+    "vhost": "main.dxp.localtest.me"
+}
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/ready", (req, res) => {
+	res.send('READY');
+});
+
+
+const serverPort = 3001;
+app.listen(serverPort, () => {
+	console.log(`App listening on ${serverPort}`);
+});
+
+
+amqp.connect(connectOptions, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'login.events.postEvent';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
+

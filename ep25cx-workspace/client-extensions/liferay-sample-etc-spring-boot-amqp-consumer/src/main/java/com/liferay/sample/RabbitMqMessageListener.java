d5c70dde9fd3a5b9ef1a5185e4d6d5a172cc46d3
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2025-07-09 14:54:37 -0500

    adapt to use message queue

--- ep25cx-workspace/client-extensions/liferay-sample-etc-spring-boot-amqp-consumer/src/main/java/com/liferay/sample/RabbitMqMessageListener.java
+++ ep25cx-workspace/client-extensions/liferay-sample-etc-spring-boot-amqp-consumer/src/main/java/com/liferay/sample/RabbitMqMessageListener.java
@@ -3,15 +3,38 @@ package com.liferay.sample;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 /**
  * @author Marcel Tanuri
  */
 @Component
 public class RabbitMqMessageListener {
 
-	@RabbitListener(queues = "liferay/background_task.default")
+	@RabbitListener(queues = "C_TicketEvent.default")
 	public void receive(String message) {
 		System.out.println("Received message: " + message);
+
+
+        JSONObject messageObject = new JSONObject(message);
+        JSONObject objectEntryObject = messageObject.getJSONObject("objectEntry");
+        JSONObject valuesObject = objectEntryObject.getJSONObject("values");
+        String subject = valuesObject.getString("subject");
+
+        System.out.println("Received subject: " + subject);
 	}
+
+    	private DocumentationReferral _documentationReferral =
+		new DocumentationReferral();
+
+
+
+	@Value("${com.liferay.lxc.dxp.mainDomain}")
+	private String _lxcDXPMainDomain;
+
+	@Value("${com.liferay.lxc.dxp.server.protocol}")
+	private String _lxcDXPServerProtocol;
 
 }
